fc9891ca25e1b44e7bdc7a10ff4fb01e980396e4
aleojiang <aleojiang@gmail.com>
2014-07-07 14:39:36 +0800

    for test purpose

--- src/scripts/testenv.sh
+++ src/scripts/testenv.sh
@@ -1,41 +1,74 @@
 #!/bin/bash
 
-datacenter="idc"
-targetrawfolder="/mnt/backup/bic/raw/${datacenter}/"
-t1="/mnt/backup/bic/raw/idc"
-t2="/mnt/backup/bic/raw/mdc"
+targetrawfolder="/mnt/backup/bic"
+rm -rf ${targetrawfolder}
 
-rm -rf /mnt/backup/bic
-mkdir -p /mnt/backup/bic/in
-mkdir -p /mnt/backup/bic/out
-mkdir -p /mnt/backup/bic/conf
-mkdir -p /mnt/backup/bic/dim/idc/user
-mkdir -p /mnt/backup/bic/dim/idc/page
-mkdir -p /mnt/backup/bic/dim/mdc/user
-mkdir -p /mnt/backup/bic/dim/mdc/page
+datacenters=(idc mdc)
+datapaths=(raw run conf out dim)
+dims=(page user event)
+dts=(cccdr ccstats cgcdr cgstats mucdr mustats submscdr submsstats huaweicdr)
 
-datatypes=(cccdr ccstats cgcdr cgstats \
-mucdr mustats submscdr submsstats huaweicdr)
-
-mkdir -p ${targetrawfolder}
-for datatype in "${datatypes[@]}"
+for path in "${datapaths[@]}"
 do
-    mkdir -p ${targetrawfolder}${datatype}
+    for dc in "${datacenters[@]}"
+    do
+        if [ "${path}" == "dim" ]; then
+            for dim in "${dims[@]}"
+            do
+                mkdir -p ${targetrawfolder}/$path/${dc}/${dim}
+                currenttime=$(date +%s)
+                touch ${targetrawfolder}/${path}/${dc}/${dim}/${currenttime}.dim
+            done
+        else
+            for dt in "${dts[@]}"
+            do
+                mkdir -p ${targetrawfolder}/${path}/${dc}/${dt}
+            done
+        fi
+    done
 done
-cp -r ${t1} ${t2}
 
-#prepare global conf
-psc=/media/D/workspace/bic2demo/src/test/resources/data/
-mkdir -p /mnt/backup/bic/app
-cp ${psc}cccdr.properties /mnt/backup/bic/conf/
+testsrc=/home/patrick.jiang/opensources/bic2demo/src/test/resources/data
+cp ${testsrc}/raw/* /mnt/backup/bic/raw/idc/cccdr/
+cp ${testsrc}/raw/* /mnt/backup/bic/raw/mdc/cccdr/
+cp ${testsrc}/cccdr.properties /mnt/backup/bic/conf/idc/cccdr/
+cp ${testsrc}/cccdr.properties /mnt/backup/bic/conf/mdc/cccdr/
 
-#prepare raw data
-cp ${psc}raw/* /mnt/backup/bic/raw/idc/cccdr/
-cp ${psc}raw/*.properties /mnt/backup/bic/raw/idc/cccdr/
-touch  /mnt/backup/bic/dim/idc/user/1.dim
-touch  /mnt/backup/bic/dim/idc/page/1.dim
-touch  /mnt/backup/bic/dim/mdc/user/1.dim
-touch  /mnt/backup/bic/dim/mdc/page/1.dim
+#targetrawfolder="/mnt/backup/bic/raw/${datacenter}/"
+#t1="/mnt/backup/bic/raw/idc"
+#t2="/mnt/backup/bic/raw/mdc"
+#
+#rm -rf /mnt/backup/bic
+#mkdir -p /mnt/backup/bic/in
+#mkdir -p /mnt/backup/bic/out
+#mkdir -p /mnt/backup/bic/conf
+#mkdir -p /mnt/backup/bic/dim/idc/user
+#mkdir -p /mnt/backup/bic/dim/idc/page
+#mkdir -p /mnt/backup/bic/dim/mdc/user
+#mkdir -p /mnt/backup/bic/dim/mdc/page
+#
+#datatypes=(cccdr ccstats cgcdr cgstats \
+#mucdr mustats submscdr submsstats huaweicdr)
+#
+#mkdir -p ${targetrawfolder}
+#for datatype in "${datatypes[@]}"
+#do
+#    mkdir -p ${targetrawfolder}${datatype}
+#done
+#cp -r ${t1} ${t2}
+#
+##prepare global conf
+#psc=/media/d/workspace/bic2demo/src/test/resources/data/
+#mkdir -p /mnt/backup/bic/app
+#cp ${psc}cccdr.properties /mnt/backup/bic/conf/
+#
+##prepare raw data
+#cp ${psc}raw/* /mnt/backup/bic/raw/idc/cccdr/
+#cp ${psc}raw/*.properties /mnt/backup/bic/raw/idc/cccdr/
+#touch  /mnt/backup/bic/dim/idc/user/1.dim
+#touch  /mnt/backup/bic/dim/idc/page/1.dim
+#touch  /mnt/backup/bic/dim/mdc/user/1.dim
+#touch  /mnt/backup/bic/dim/mdc/page/1.dim
 
 
 
